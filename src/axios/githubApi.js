4aa176f17d715899e9ee0535c8ab8f8c415660c0
Tania Quijano <tquijano@unal.edu.co>
2023-02-23 03:15:09 -0500

    cors arreglado :D

--- src/axios/githubApi.js
+++ src/axios/githubApi.js
@@ -2,14 +2,20 @@ import axios from "axios";
 
 const githubApi = axios.create({
   baseURL: 'https://github.com',
+  mode: 'no-cors',
+  // withCredentials: true,
 });
 
-// githubApi.interceptors.request.use((config) => {
-//   config.headers = {
-//     ...config.headers,
-//     Authorization: `token ${localStorage.getItem("token")}`,
-// };
-// return config;
-// });
+githubApi.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    // 'Access-Control-Allow-Credentials': true,
+    // 'Access-Control-Allow-Origin':'*',
+    // 'Access-Control-Allow-Headers':'Origin, Authorization, X-Requested-With, Content-Type, Accept',
+    // 'Access-Control-Allow-Methods':'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+    // Authorization: `token ${localStorage.getItem("token")}`,
+};
+return config;
+});
 
 export default githubApi;
